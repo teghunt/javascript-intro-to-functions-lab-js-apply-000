05f6a0a6b7fb368b2b1678f6cbd47ed3d823c881
Tom Hunt <teghunt@gmail.com>
2019-01-10 12:41:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
+  if (string === string.toLowerCase()) {
+    return ("I can't hear you!");
+  } if (string === string.toUpperCase()); {
+    return ("YES INDEED!");
+  } if (string === 'I love you, Grandma.'); {
+    return ("I love you, too.");
+  }
+}
+sayHiToGrandma(whisper('heLlo'));
+sayHiToGrandma(shout('HEllO'));
+sayHiToGrandma("I love you, Grandma."); ```
+
+### error message
+
+``` 1) sayHiToGrandma(string) returns "I love you, too." if `string` is "I love you, Grandma."`:
+      Error: Expected 'YES INDEED!' to equal 'I love you, too.'
+      + expected - actual
+      -YES INDEED!
+      +I love you, too.
+      at assert (node_modules/expect/lib/assert.js:29:9)
+      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
+      at Context.<anonymous> (test/index-test.js:48:52) ```
