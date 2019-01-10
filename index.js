dc44d8739012b04eb6c5a499cd43906d0bcda6b4
Tom Hunt <teghunt@gmail.com>
2019-01-10 12:43:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,5 @@ function whisper(string) {
 }
 sayHiToGrandma(whisper('heLlo'));
 sayHiToGrandma(shout('HEllO'));
-sayHiToGrandma("I love you, Grandma."); ```
+sayHiToGrandma("I love you, Grandma.");
 
-### error message
-
-``` 1) sayHiToGrandma(string) returns "I love you, too." if `string` is "I love you, Grandma."`:
-      Error: Expected 'YES INDEED!' to equal 'I love you, too.'
-      + expected - actual
-      -YES INDEED!
-      +I love you, too.
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.<anonymous> (test/index-test.js:48:52) ```
