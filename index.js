2225086486eb821b514b6f63124872d54c8ad274
Tom Hunt <teghunt@gmail.com>
2019-01-10 12:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+```javascript
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+```
